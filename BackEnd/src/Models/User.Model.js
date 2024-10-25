158b2cc40de6a0d4d7f2565e6a1b8cfc5713a8a9
Ansuhan <anushan559@gmail.com>
2024-10-25 11:52:43 +0530

    Created User Model

---
+++ BackEnd/src/Models/User.Model.js
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+import { v1 as uuidv1 } from 'uuid';
+import crypto from 'crypto';
+
+
+var userSchema=new mongoose.Schema({
+    Email:{
+        type:String,
+        required:true,
+        maxlength:32,
+        trim:true
+    },
+    HospitalName:{
+        type:String,
+        required:true,
+        maxlength:32,
+        trim:true
+    },
+    encry_password:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    salt:String,
+    Role:{
+        type:String,
+        default:0
+    },
+    Isverified:{
+        type:String,
+        default:0
+    },
+    Phno:{
+        type:Number,
+        trim:true,
+        maxlength:10,
+        required:true
+    }
+    
+},
+{timestamps:true}
+)
+
+userSchema.methods={
+    
+    autheticate: function(plainpassword) {
+        return this.securepassword(plainpassword) === this.encry_password;
+    },
+    
+    securepassword:function(plainpassword){
+        if(!plainpassword){
+            return "";
+        }
+        try{
+            return crypto
+            .createHmac("sha256", this.salt)
+            .update(plainpassword)
+            .digest("hex");
+            
+        }
+        catch(err){
+            console.log("if4")
+            return "";
+        }
+    }
+}
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._passsword=password;
+        this.salt=uuidv1();
+        this.encry_password=this.securepassword(password);
+    })
+    .get(function(){
+    return this._passsword;
+    })
+
+
+
+
+module.exports=mongoose.model("User",userSchema)
