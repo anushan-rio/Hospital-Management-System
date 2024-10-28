2b745b4383e5a12fd92a5a082931f7bd1cea4ad5
Sharusan S <sharusans@Sharusans-MacBook-Air.local>
2024-10-28 20:14:06 +0530

    Signup controller completed

--- BackEnd/src/Config/Db.Config.js
+++ BackEnd/src/Config/Db.Config.js
@@ -1,2 +1,3 @@
 
-export const dbURL = "mongodb+srv://Sharusan:Sharusan24@clustermain.mpuvv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMain";
+// export const dbURL = "mongodb+srv://Sharusan:Sharusan24@clustermain.mpuvv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMain";
+export const dbURL = "mongodb://localhost:27017/HospitalManagement"
