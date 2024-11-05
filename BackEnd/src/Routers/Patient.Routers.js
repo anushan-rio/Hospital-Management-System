a1ea556f4dc2e6cb360cad915d64119b959c43df
Sharusan S <sharusans@Sharusans-MacBook-Air.local>
2024-11-05 16:51:56 +0530

    delete amd update patient contorller

--- BackEnd/src/Routers/Patient.Routers.js
+++ BackEnd/src/Routers/Patient.Routers.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {AddPatientInfo, GetAllPatient,GetPatient} from "../Controllers/Patient.contollers.js"
+import {AddPatientInfo, GetAllPatient,GetPatient, RemovePatientData, UpdatePatientData} from "../Controllers/Patient.contollers.js"
 import {GetUserId,IsAdmin,IsAuthenticate,IsSignedIn} from "../Middleware/Auth.MiddleWare.js"
 import { GetPatientID } from '../Middleware/Patient.Middlware.js';
 
@@ -14,6 +14,11 @@ router.post("/Patient/:UserID",IsSignedIn,IsAuthenticate,AddPatientInfo);
 router.get("/Patient/GetAllUser/:UserID",IsSignedIn, IsAuthenticate, IsAdmin ,GetAllPatient)
 router.get("/Patient/GetPatient/:PatientID" ,GetPatient)
 
+//Delete Router
+router.delete("/Patient/RemovePatient/:PatientID" ,RemovePatientData)
 
+
+//Update Router
+router.put("/Patient/UpdatePatient/:PatientID" ,UpdatePatientData)
 
 export default router;
