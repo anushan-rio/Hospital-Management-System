cdb8a846731f741644f36e0dc0cd6036e54a215d
Ansuhan <68387558+anushan-rio@users.noreply.github.com>
2024-11-08 18:34:15 +0530

    Update constant.js

--- BackEnd/src/constant.js
+++ BackEnd/src/constant.js
@@ -2,6 +2,6 @@ export const PORT = 3000;
 
 export const SUCESS_MESSAGE= "Data Saved Successfully"
 export const CATCH_MESSAGE= "Data Not Saved ,Try Again !!"
-export const EMAIL="anushandevstack@gmail.com"
-export const EMAIL_PASSWORD="qycc spsj oxst zgys"
+export const EMAIL=""
+export const EMAIL_PASSWORD=""
 export const ACCESS_TOKEN_SECRET="your access token secret"
