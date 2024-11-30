ab44d0076b69639d793a0eb7b31f2c6b093a832d
Ansuhan <68387558+anushan-rio@users.noreply.github.com>
2024-11-30 19:02:28 +0530

    Update server.js

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 
 import mongoose from 'mongoose';
 import { dbURL } from './Config/Db.Config.js';
-import {PORT} from "./constant.js"
 import bodyParser from 'body-parser';
 const app= express();
 
@@ -17,7 +16,7 @@ mongoose.connect(dbURL)
 app.use(bodyParser.json());
 app.use(cors());
 
-
+const PORT =process.env.PORT || 8080
 //My Routes
 import authRoutes from "./Routers/Auth.Routers.js";
 import patientRoutes from "./Routers/Patient.Routers.js";
